aa13c4c0ef622887e9c03af7c07101a64db2e463
chrizlm <42278574+chrizlm@users.noreply.github.com>
2022-05-19 16:08:17 +0300

    Update Tasks.ts

--- contract/assembly/Modules/Tasks.ts
+++ contract/assembly/Modules/Tasks.ts
@@ -1,6 +1,12 @@
+import { TaskStatus } from '../utils/enums';
 
 @nearBindgen
 export class Task {
   title: String;
-  status: String;
+  status: TaskStatus;
+
+  constructor(title: String) {
+    this.title = title;
+    this.status = TaskStatus.PENDING;
+  }
 }
